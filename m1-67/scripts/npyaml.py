535c68a5f7322aa269282a16de2b3abfa94b68b7
William Henney <will@henney.org>
2024-06-22 23:55:22 -0600

    Working towards the psf fitting
    Going pretty well. Looks like empirical psf is best

--- m1-67/scripts/npyaml.py
+++ m1-67/scripts/npyaml.py
@@ -1,25 +1,41 @@
-"""
-Human-readable representation of numpy data types for YAML.
+"""Human-readable representation of numpy data types for YAML.
 
-Usage:
+Example Usage:
 
 > import npyaml
 > npyaml.register_all()
 > ...
 > yaml.dump(DATA_WITH_NUMPY_ARRAYS_AND_SCALARS)
 
-This is for when we want a transparent, easy-to-read representation of
-numpy arrays and scalars in a YAML dump.  Note that this has the
-disadvantage that the data will not be read back in as numpy arrays,
-but as lists and python scalars.
+This package is for when you want a transparent, easy-to-read
+representation of numpy arrays and scalars in a YAML dump.  
 
-William Henney 2023, originally based on code from answer to this
+Authors: (c) Felix Kloss 2023, William Henney 2023, 2024
+Maintainer: William Henney <will@henney.org>
+License: MIT
+
+Originally based on the answer by Felix (@luator) to this
 StackOverflow question:
 
 https://stackoverflow.com/questions/75508283/dump-numpy-array-to-yaml-as-regular-list
 
-But I have added the ability to handle scalar arrays and to deal with
-all the subtypes of numpy floating and integer types.
+I have added the ability to handle scalar arrays and to deal with all
+the subtypes of numpy floating and integer types. This is tested with
+masked arrays, which are dumped with the masked elements replaced by
+"null".
+
+Other potential corner cases, such as complex arrays, or object arrays
+have not been tested, so use with care!
+
+If you use yaml.safe_dump() instead of yaml.dump(), an exception will
+be raised for any data types that do not have a human-readable YAML
+representation.
+
+One potential disadvantage is that the data will NOT be read back in
+as numpy arrays (by yaml.safe_load(), for instance), but instead as
+lists and python scalars. If required, you can explicitly convert the
+data back to numpy arrays using np.array().
+
 """
 
 import numpy as np
@@ -44,7 +60,7 @@ def _ndarray_representer(dumper: yaml.Dumper, array: np.ndarray) -> yaml.Node:
         elif isinstance(value, bool):
             return dumper.represent_bool(value)
         else:
-            # Just in cas I have forgotten something
+            # Just in case I have forgotten something
             raise ValueError("Unsupported type: %s" % type(value))
 
 
@@ -73,6 +89,9 @@ if __name__ == "__main__":
     # Test it works
     a = np.array([1, 2, 3])
     print(yaml.dump({"ndarray": a}))
+    aa = np.arange(9).reshape(3, 3)
+    print(yaml.dump({"2-d ndarray": aa}, default_flow_style=False))
+    print(yaml.dump({"inline 2-d ndarray": aa}, default_flow_style=True))
     i = np.array(1)
     print(yaml.dump({"scalar int ndarray": i}))
     x = np.array(1.0)
@@ -81,3 +100,6 @@ if __name__ == "__main__":
     print(yaml.dump({"np.int64": i}))
     x = np.float64(1.0)
     print(yaml.dump({"np.float64": x}))
+    am = np.ma.array([1, 2, 3], mask=[0, 1, 0])
+    print(yaml.dump({"masked array": am}))
+    
